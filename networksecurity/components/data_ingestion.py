0be0804dfa16c458320a77cee91fd9f4fadb6e8d
shakeabhi7 <kumarabhishekt7@gmail.com>
2025-08-29 12:49:11 +0530

    Data Validation

--- networksecurity/components/data_ingestion.py
+++ networksecurity/components/data_ingestion.py
@@ -30,62 +30,72 @@ class DataIngestion:
         """
         Read data from mongodb
         """
-
         try:
-            database_name = self.data_ingestion_config.database_name
-            collection_name = self.data_ingestion_config.collection_name
-            self.mongo_client = pymongo.MongoClient(MONGO_DB_URL)
-            collection = self.mongo_client[database_name][collection_name]
+            database_name=self.data_ingestion_config.database_name
+            collection_name=self.data_ingestion_config.collection_name
+            self.mongo_client=pymongo.MongoClient(MONGO_DB_URL)
+            collection=self.mongo_client[database_name][collection_name]
 
-            df = pd.DataFrame(list(collection.find()))
-            if"_id" in df.columns.to_list():
+            df=pd.DataFrame(list(collection.find()))
+            if "_id" in df.columns.to_list():
                 df=df.drop(columns=["_id"],axis=1)
             
             df.replace({"na":np.nan},inplace=True)
             return df
-        
         except Exception as e:
             raise CustomException
         
 
-    def export_data_into_feature_store(self,dataframe:pd.DataFrame):
+    def export_data_into_feature_store(self,dataframe: pd.DataFrame):
         try:
-            feature_store_file_path = self.data_ingestion_config.feature_store_file_path
-            # Creating Folder
+            feature_store_file_path=self.data_ingestion_config.feature_store_file_path
+            #creating folder
             dir_path = os.path.dirname(feature_store_file_path)
             os.makedirs(dir_path,exist_ok=True)
             dataframe.to_csv(feature_store_file_path,index=False,header=True)
             return dataframe
-
+            
         except Exception as e:
             raise CustomException(e,sys)
         
-    def split_data_as_train_test(self,dataframe:pd.DataFrame):
+    def split_data_as_train_test(self,dataframe: pd.DataFrame):
         try:
             train_set, test_set = train_test_split(
-                dataframe,test_size = self.data_ingestion_config.train_test_split_ratio
+                dataframe, test_size=self.data_ingestion_config.train_test_split_ratio
             )
             logging.info("Performed train test split on the dataframe")
-            logging.info("Exited split_data_as_train_test method of Data_Ingestion class")
 
+            logging.info(
+                "Exited split_data_as_train_test method of Data_Ingestion class"
+            )
+            
             dir_path = os.path.dirname(self.data_ingestion_config.training_file_path)
+            
+            os.makedirs(dir_path, exist_ok=True)
+            
+            logging.info(f"Exporting train and test file path.")
+            
+            train_set.to_csv(
+                self.data_ingestion_config.training_file_path, index=False, header=True
+            )
 
-            os.makedirs(dir_path,exist_ok=True)
+            test_set.to_csv(
+                self.data_ingestion_config.testing_file_path, index=False, header=True
+            )
+            logging.info(f"Exported train and test file path.")
 
-            train_set.to_csv(self.data_ingestion_config.training_file_path,index=True,header=True)
-            test_set.to_csv(self.data_ingestion_config.testing_file_path,index=False,header=True)
-            logging.info(f"Exported train and test file path")
+            
         except Exception as e:
             raise CustomException(e,sys)
 
     def initiate_data_ingestion(self):
         try:
-            dataframe = self.export_collection_as_dataframe()
-            dataframe = self.export_data_into_feature_store(dataframe)
+            dataframe=self.export_collection_as_dataframe()
+            dataframe=self.export_data_into_feature_store(dataframe)
             self.split_data_as_train_test(dataframe)
-            dataingestionartifact = DataIngestionArtifact(trained_file_path = self.data_ingestion_config.training_file_path,
-            test_file_path=self.data_ingestion_config.testing_file_path)
-            
+            dataingestionartifact=DataIngestionArtifact(trained_file_path=self.data_ingestion_config.training_file_path,
+                                                        test_file_path=self.data_ingestion_config.testing_file_path)
             return dataingestionartifact
+
         except Exception as e:
-            raise CustomException
+            raise CustomException(e,sys)
