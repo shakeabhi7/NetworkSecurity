a2442fe827aa32e5a89c1a55af70754129af8f1a
shakeabhi7 <kumarabhishekt7@gmail.com>
2025-09-05 13:58:11 +0530

    Model trainer and Mlflow experiment

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from networksecurity.entity.config_entity import DataIngestionConfig,DataValidat
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 import sys
 
+from networksecurity.components.model_trainer import ModelTrainer
+from networksecurity.entity.config_entity import ModelTrainerConfig
+
 if __name__=='__main__':
     try:
 
@@ -34,6 +37,15 @@ if __name__=='__main__':
         data_transformation_artifact=data_transformation.initiate_data_transformation()
         print(data_transformation_artifact)
         logging.info("data Transformation completed")
+
+        # Model Training
+        logging.info("Model Training Started")
+        model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+
+        logging.info("Model Training artifact created")
+
 
     
     except Exception as e:
--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -16,8 +16,8 @@ TEST_FILE_NAME: str = "test.csv"
 
 SCHEMA_FILE_PATH = os.path.join("data_schema", "schema.yaml")
 
-# SAVED_MODEL_DIR =os.path.join("saved_models")
-# MODEL_FILE_NAME = "model.pkl"
+SAVED_MODEL_DIR =os.path.join("saved_models")
+MODEL_FILE_NAME = "model.pkl"
 
 
 
@@ -59,3 +59,15 @@ DATA_TRANSFORMATION_IMPUTER_PARAMS: dict = {
 
 DATA_TRANSFORMATION_TRAIN_FILE_PATH: str = "train.py"
 DATA_TRANSFORMATION_TEST_FILE_PATH: str = "test.py"
+
+"""
+Model Trainer related constant start with MODEL TRAINER VAR NAME
+"""
+
+MODEL_TRAINER_DIR_NAME: str = "model_trainer"
+MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
+MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
+MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
+MODEL_TRAINER_OVER_FIITING_UNDER_FITTING_THRESHOLD: float = 0.05
+
+TRAINING_BUCKET_NAME = "netwworksecurity"
